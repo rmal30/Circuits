e75e220d4db5cdef51816396da51c61031653565
Rohan <rm18349276@gmail.com>
2021-06-14 19:06:27 +1000

    Added nodemon, fixed startup script

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8000;
 
 app.use(express.static("public"));
 app.get('/favicon.ico', express.static('favicon.ico'));
-app.use("/styles", express.static(`${__dirname}/styles`));
-app.use("/scripts", express.static(`${__dirname}/scripts`));
-app.use("/images", express.static(`${__dirname}/images`));
+app.use("/styles", express.static(`styles`));
+app.use("/scripts", express.static(`scripts`));
+app.use("/images", express.static(`images`));
 
 
 const server = app.listen(PORT, () => {

6b90b8365c83cdda4dcdaea33ef8c47254822859
Rohan <rm18349276@gmail.com>
2021-06-14 19:09:50 +1000

    Added impedance tests, bug fixes

--- scripts/analysis/impedance.js
+++ scripts/analysis/impedance.js
@@ -9,14 +9,12 @@ import {COMPONENT_TYPES} from "../config/components.js";
  */
 export function getImpedance(hertz, componentType, componentValue) {
     const freq = hertz * Math.PI * 2;
-    const capacitorScale = 10e6;
-    const inductorScale = 10e-3;
-    const diodeMinImpedance = 1e-100;
+    const capacitorScale = 1e6;
+    const inductorScale = 1e-3;
     switch (componentType) {
         case COMPONENT_TYPES.RESISTOR: return componentValue;
         case COMPONENT_TYPES.CAPACITOR: return [0, -capacitorScale / (componentValue * freq)];
         case COMPONENT_TYPES.INDUCTOR: return [0, inductorScale * componentValue * freq];
-        case COMPONENT_TYPES.DIODE: return componentValue ? diodeMinImpedance : Infinity;
-        default: return 0;
+        default: throw new Error("Unknown component");
     }
 }

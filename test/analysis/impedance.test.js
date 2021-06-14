8fd475b77efd53013cea60c53d3ca130c1f82db4
Rohan <rm18349276@gmail.com>
2021-06-14 19:09:50 +1000

    Added impedance tests, bug fixes

---
+++ test/analysis/impedance.test.js
@@ -0,0 +1,34 @@
+import {getImpedance} from "../../public/scripts/analysis/impedance.js";
+import assert from "assert";
+import { COMPONENT_TYPES } from "../../public/scripts/config/components.js";
+
+describe("Impedance calculation", () => {
+    describe("Resistance", () => {
+        assert.strictEqual(getImpedance(100, COMPONENT_TYPES.RESISTOR, 200), 200);
+    });
+
+    describe("Capacitance", () => {
+        it("Zero freq", () => {
+            assert.deepStrictEqual(getImpedance(0, COMPONENT_TYPES.CAPACITOR, 1), [0, -Infinity]);
+        });
+
+        it("50Hz", () => {
+            assert.deepStrictEqual(getImpedance(50, COMPONENT_TYPES.CAPACITOR, 1), [0, -3183.0988618379065]);
+        });
+
+    });
+
+    describe("Inductance", () => {
+        it("Zero freq", () => {
+            assert.deepStrictEqual(getImpedance(0, COMPONENT_TYPES.INDUCTOR, 1), [0, 0]);
+        });
+
+        it("50Hz", () => {
+            assert.deepStrictEqual(getImpedance(50, COMPONENT_TYPES.INDUCTOR, 1), [0, 0.3141592653589793]);
+        });
+    });
+
+    it("Throw error if unknown component", () => {
+        assert.throws(() => getImpedance(100, COMPONENT_TYPES.DIODE, 0));
+    })
+});
